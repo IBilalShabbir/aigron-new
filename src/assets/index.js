d9370f2ee6532c9ac9df4ef77bd8b94165b39062
M Abrar Khalid <abrarprince471@gmail.com>
2022-11-10 14:33:17 +0500

    aboutus__banner

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,3 @@
 export { default as react } from "./react.svg";
+export { default as aboutusbanner } from "./aboutusbanner.png";
+export { default as aboutusbannerlogo } from "./aboutusbannerlogo.png";
--- src/screens/about.jsx
+++ src/screens/about.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { react } from "../assets";
+import { aboutusbanner, aboutusbannerlogo } from "../assets";
 
 export default function About() {
   return (
-    <div>
-      <img src={react} alt="react" />
-    </div>
+    <>
+      <div className="aboutus__banner">
+        <img src={aboutusbanner} alt="aboutusbanner" />
+      </div>
+      <div className="aboutus__banner__content">
+        <div className="aboutus__banner__logo">
+          <img src={aboutusbannerlogo} alt="aboutusbannerlogo" />
+        </div>
+        <button className="aboutus__banner__button">Let us support you</button>
+      </div>
+    </>
   );
 }
