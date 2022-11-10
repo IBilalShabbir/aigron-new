510f0043b5a22e04a7331b5d1e4405c833755f11
M Abrar Khalid <abrarprince471@gmail.com>
2022-11-10 18:59:44 +0500

    aboutus sections

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,7 @@
 export { default as react } from "./react.svg";
 export { default as aboutusbanner } from "./aboutusbanner.png";
 export { default as aboutusbannerlogo } from "./aboutusbannerlogo.png";
+export { default as aboutusleft } from "./aboutusleft.png";
+export { default as aboutusfirstsection } from "./aboutusfirstsection.png";
+export { default as aboutussecondsection } from "./aboutussecondsection.png";
+export { default as aboutusright } from "./aboutusright.png";
