cc4aaddfd5530bb7363c3c849b3dbf442f9f148a
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-11-11 11:24:18 +0500

    base header

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,4 @@
 export { default as homeSectionBanner } from "./homeSectionBanner.mp4";
 export { default as homeSectionBg } from "./homeSectionBg.png";
+export { default as logo } from "./logo.svg";
+export { default as usaFlog } from "./usaFlog.png";
