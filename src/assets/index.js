d679594b36b3101631a6cf3ceb4d99a8d3980041
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-11-11 10:41:48 +0500

    base done

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,2 @@
-export { default as react } from "./react.svg";
-export { default as aboutusbanner } from "./aboutusbanner.png";
-export { default as aboutusbannerlogo } from "./aboutusbannerlogo.png";
-export { default as aboutusleft } from "./aboutusleft.png";
-export { default as aboutusfirstsection } from "./aboutusfirstsection.png";
-export { default as aboutussecondsection } from "./aboutussecondsection.png";
-export { default as aboutusright } from "./aboutusright.png";
 export { default as homeSectionBanner } from "./homeSectionBanner.mp4";
 export { default as homeSectionBg } from "./homeSectionBg.png";
-export { default as frame } from "./frame.png";
