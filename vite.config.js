94f073ee9695dea72359badb0ca9751eff6a99bb
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-11-14 18:05:56 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -30,48 +30,5 @@ export default defineConfig({
       algorithm: "brotliCompress",
       threshold: 100,
     }),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 8,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 8,
-      },
-      mozjpeg: {
-        quality: 20,
-      },
-      pngquant: {
-        quality: [0.7, 0.8],
-        speed: 4,
-      },
-      webp: {
-        quality: 70,
-      },
-      svgo: {
-        multipass: true,
-        plugins: [
-          {
-            name: "removeViewBox",
-          },
-          {
-            name: "minifyStyles",
-          },
-          {
-            name: "removeMetadata",
-          },
-          {
-            name: "removeUselessStrokeAndFill",
-          },
-          {
-            name: "reusePaths",
-          },
-          {
-            name: "removeEmptyAttrs",
-            active: true,
-          },
-        ],
-      },
-    }),
   ],
 });
